43a45006262ac0c6ab96cd67a6c68378fd5cd4cb
AkiIto <ito.aki@da-vinci.studio.net>
2023-01-18 09:16:18 +0900

    他のプロバイダーを利用

--- lib/provider.dart
+++ lib/provider.dart
@@ -2,3 +2,19 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final cityProvider = Provider((ref) => 'London');
 final countryProvider = Provider((ref) => 'England');
+
+// https://riverpod.dev/ja/docs/concepts/reading
+final countProvider = StateNotifierProvider<Counter, int>((ref) {
+  return Counter(ref);
+});
+
+class Counter extends StateNotifier<int> {
+  Counter(this.ref) : super(0);
+  final Ref ref;
+  
+  void increment(){
+    // countProviderはrefによりrepositoryProviderを使えるようになった
+    final repository = ref.read(repositoryProvider);
+    repository.post();
+  }
+}
