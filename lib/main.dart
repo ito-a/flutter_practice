d176e7718d216b427806c7bd9d4ddec71a2ce72e
AkiIto <ito.aki@da-vinci.studio.net>
2023-01-20 12:55:11 +0900

    TodoListをつくってみよう

--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,57 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final helloWorldProvider = Provider((_) => 'Hello World');
 
+// Todoリストを作ってみよう!
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-final cityProvider = StateProvider((ref) => '');
+class Todo {
+  Todo({required this.description, required this.id, this.isCompleted = false});
 
-// プロバイダのステートを組み合わせる
-final weatherProvider = FutureProvider((ref) {
-  final city = ref.watch(cityProvider);
-  return fetchWeather(city);
-});
-
-String fetchWeather(String city) {
-  switch (city) {
-    case 'London':
-      return '晴れ';
-    case 'Paris':
-      return '曇り';
-    case 'Roma':
-      return '雨';
-  }
-  return ' 各都市の天気はなんでしょう?';
+  String description;
+  String id;
+  bool isCompleted;
 }
+
+class TodoList extends StateNotifier<List<Todo>> {
+  TodoList([List<Todo>? initialTodos]) : super(initialTodos ?? []);
+}
+
+enum Filter {
+  all,
+  completed,
+  uncompleted,
+}
+
+final todoListProvider = StateNotifierProvider<TodoList, List<Todo>>((ref) {
+  return TodoList([
+    Todo(description: 'TEST1', id: '1'),
+    Todo(description: 'TEST2', id: '2'),
+    Todo(description: 'TEST3', id: '3')
+  ]);
+});
+final filterProvider = StateProvider((ref) => Filter.all);
+
+final filteredTodoListProvider = Provider<List<Todo>>((ref) {
+  final filter = ref.watch(filterProvider);
+  final todos = ref.watch(todoListProvider);
+  switch (filter) {
+    case Filter.all:
+      return todos;
+    case Filter.completed:
+      return todos.where((element) => element.isCompleted).toList();
+    case Filter.uncompleted:
+      return todos.where((element) => !element.isCompleted).toList();
+  }
+});
 
 class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    AsyncValue weather = ref.watch(weatherProvider);
+    final filteredTodoList = ref.watch(filteredTodoListProvider);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -40,23 +61,30 @@ class MyApp extends ConsumerWidget {
         ),
         body: Column(
           children: [
-            weather.when(
-              data: (value) {
-                return Text(value);
-              },
-              error: (err, stack) => Text('Error: $err'),
-              loading: () => const CircularProgressIndicator(),
+            ListView.builder(
+                shrinkWrap: true,
+                itemCount: filteredTodoList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Row(
+                    children: [
+                      Text(filteredTodoList[index].description),
+                    ],
+                  );
+                }),
+            ElevatedButton(
+              onPressed: () => ref.read(filterProvider.notifier).state = Filter.all,
+              child: const Text('All'),
             ),
             ElevatedButton(
-              onPressed: () => ref.read(cityProvider.notifier).state = 'London',
-              child: const Text('London'),
+              onPressed: () => ref.read(filterProvider.notifier).state = Filter.completed,
+              child: const Text('Completed'),
             ),
             ElevatedButton(
-              onPressed: () => ref.read(cityProvider.notifier).state = 'Paris',
-              child: const Text('Paris'),
+              onPressed: () => ref.read(filterProvider.notifier).state = Filter.uncompleted,
+              child: const Text('UnCompleted'),
             ),
             ElevatedButton(
-              onPressed: () => ref.refresh(cityProvider),
+              onPressed: () => ref.refresh(filterProvider),
               child: const Text('Refresh'),
             ),
           ],
